b82472b5a1dc25e3e8c87a1fb926448ad7a0a1e4
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-05-17 18:31:13 +0700

    WIP(home banner parallax):Add pagination demo

--- src/components/shared/parallax/ScrollParallax.jsx
+++ src/components/shared/parallax/ScrollParallax.jsx
@@ -1,66 +1,48 @@
-import Image from "next/image";
 
-const ScrollParallax = () => {
+const ScrollParallax= () => {
   return (
-    <section>
-      <div>
-        <div className="card text-8xl -m-40 h-[125vh] bg-[#089193] sticky top-0">
-          {/* <Image
-            className="absolute w-full h-[150vh] z-30"
-            src="https://globalintergold.info/_files/70/39/1d/91/e0/70391d91e0b8bf130acf04b2d1eb1cc9.jpg"
-            alt=""
-          /> */}
-        </div>
-        <div className="card text-8xl h-screen bg-[#0b48298b] sticky top-0">
-          2
-        </div>
-        <div className="card text-8xl h-screen bg-[#77217a] sticky top-0">
-          {/* <Image
-            className="absolute w-full h-full z-30"
-            src="https://globalintergold.info/_files/70/39/1d/91/e0/70391d91e0b8bf130acf04b2d1eb1cc9.jpg"
-            alt=""
-          /> */}
+    <section className="relative">
+      <div className="relative ">
+   
+      <div className="section text-8xl -m-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
+        <div className="absolute inset-0 bg-[#000000a7] w-full h-full z-30"></div>
+        <img className="absolute w-full h-[150vh] z-20 bg-blend-screen" src="https://globalintergold.info/_files/70/39/1d/91/e0/70391d91e0b8bf130acf04b2d1eb1cc9.jpg" alt="" />
+        <div className=" absolute inset-0 flex flex-col justify-center items-center text-white z-30">
+        <h1 className="text-6xl">luxury accessories</h1>
+        <button className="text-xl bg-[black] py-3 px-5 rounded-lg mt-8" >Discover Now</button>
         </div>
       </div>
-      <div className="card text-8xl h-screen bg-[#50acae] ">Footer</div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque
-        corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam
-        possimus pariatur quisquam alias.
-      </p>
+      {/* 2 */}
+      <div className="section text-8xl mt-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
+      
+    
+        <div className="absolute inset-0 bg-[#00000084] w-full h-full z-30"></div>
+        <img className="absolute w-full h-[150vh] z-20" src="https://globalintergold.info/_files/70/39/1d/91/e0/70391d91e0b8bf130acf04b2d1eb1cc9.jpg" alt="" />
+        <div className=" absolute inset-0 flex flex-col justify-center items-center text-white z-30">
+        <h1 className="text-6xl">luxury accessories</h1>
+        <button className="text-xl bg-[black] py-3 px-5 rounded-lg mt-8" >Discover Now</button>
+        </div>
+      </div>
+    {/* 3 */}
+    <div className="section text-8xl mt-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
+        <div className="absolute inset-0 bg-[#00000084] w-full h-full z-30"></div>
+        <img className="absolute w-full h-[150vh] z-20" src="https://globalintergold.info/_files/70/39/1d/91/e0/70391d91e0b8bf130acf04b2d1eb1cc9.jpg" alt="" />
+        <div className=" absolute inset-0 flex flex-col justify-center items-center text-white z-30">
+        <h1 className="text-6xl">luxury accessories</h1>
+        <button className="text-xl bg-[black] py-3 px-5 rounded-lg mt-8" >Discover Now</button>
+        </div>
+      </div>
+
+      </div>
+      <div className="card text-8xl h-screen bg-[#50acae] mt-64">Footer</div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt reiciendis ipsa dolores minus voluptas soluta quis temporibus eaque corrupti facilis voluptatem reprehenderit, nobis sunt velit ullam possimus pariatur quisquam alias.</p>
     </section>
   );
 };
 
-export default ScrollParallax;
+export default ScrollParallax; 
--- src/hooks/useScrollObserver.jsx
+++ src/hooks/useScrollObserver.jsx
@@ -1,42 +1,16 @@
-'use client'
-
-import ScrollParallax from "@/components/shared/parallax/ScrollParallax";
-import Pagination from "./pagination";
-import { useEffect } from "react";
-
-const Banner= () => {
-
-
-
-
-
-
-  useEffect(()=>{
-
-
-
-
-
-
-
-
-
 // Assuming you have an array of section elements and pagination dots
 const sections = document.querySelectorAll('.section');
 const paginationDots = document.querySelectorAll('.pagination-dot');
-
-
-
 
 // Function to change the color of the pagination dot
 const changeDotColor = (activeIndex) => {
   // Reset all dots to default color
   paginationDots.forEach(dot => {
-    dot.style.backgroundColor = 'white'; // Replace with your default color
+    dot.style.backgroundColor = ''; // Replace with your default color
   });
 
   // Change the color of the active dot
-  paginationDots[activeIndex].style.backgroundColor = 'red'; // Replace with your active color
+  paginationDots[activeIndex].style.backgroundColor = 'white'; // Replace with your active color
 };
 
 // Function to handle scroll event
@@ -48,7 +22,13 @@ const onScroll = () => {
       activeIndex = index;
     }
   });
+  
   changeDotColor(activeIndex);
+
+
+
+
+
 };
 
 // Attach the scroll event listener
@@ -59,31 +39,8 @@ window.addEventListener('scroll', onScroll);
 
 
 
-  },[])
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return (
-    <section className="relative">
-        <ScrollParallax/>
-      <Pagination/>
-      </section>
-  );
-};
-
-export default Banner; 
