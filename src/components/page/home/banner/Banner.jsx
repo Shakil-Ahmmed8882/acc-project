15bee06dba2fc4137d2dea833336d7c51cf712a4
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-05-17 22:41:49 +0700

    fix(home-banner):fix sticky content in parallax effect

--- src/components/page/home/banner/Banner.jsx
+++ src/components/page/home/banner/Banner.jsx
@@ -1,21 +1,20 @@
-'use client'
+"use client";
 
 import ScrollParallax from "@/components/shared/parallax/ScrollParallax";
 import Pagination from "./pagination";
 import { useEffect } from "react";
 import useScrollObserver from "@/hooks/useScrollObserver";
 
-const Banner= () => {
-
-
-  useScrollObserver()
+const Banner = () => {
+  // it's for active pagination marking
+  useScrollObserver();
 
   return (
     <section className="relative">
-        <ScrollParallax/>
-      <Pagination/>
-      </section>
+      <ScrollParallax />
+      <Pagination />
+    </section>
   );
 };
 
-export default Banner; 
+export default Banner;
