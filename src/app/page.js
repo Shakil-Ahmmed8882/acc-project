0b2b150d862a89153d5d036deb9c4af4c8c04c9b
Forhad96 <forhadairdrop@gmail.com>
2024-05-17 22:59:29 +0600

    Add:flagshipProduct done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import Banner from "@/components/page/home/banner/Banner";
+import FlagshipProducts from "@/components/page/home/flagshipProducts/FlagshipProducts";
+import ProductSlider from "@/components/page/home/flagshipProducts/ProductSlider";
 import Newsletter from "@/components/shared/newsletter/Newsletter";
+import Fslider from "@/components/test/Fslider";
+import Fslider2 from "@/components/test/Fslider2/Fslider2";
 
 const Home = () => {
   return (
     <main className="min-h-[calc(100vh)]">
       {/* <Banner /> */}
-<Newsletter/>
+{/* <Newsletter/> */}
+{/* <FlagshipProducts/> */}
+{/* <Fslider2/> */}
+<ProductSlider/>
     </main>
   );
 };
