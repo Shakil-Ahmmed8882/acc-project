017eadc16f766ac6000d7d23a3a9fa17b6f57637
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-05-18 17:50:23 +0700

    fix(HOME):fix banner responsiveness

--- src/components/shared/parallax/ScrollParallax.jsx
+++ src/components/shared/parallax/ScrollParallax.jsx
@@ -1,17 +1,22 @@
+
+
 import ParallaxContents from "./ParallaxContents";
+
+import parallaxImg1 from '@/assets/img/home/hero/parallax1.jpg'
+import parallaxImg2 from '@/assets/img/home/hero/parallax2.jpg'
 
 const ScrollParallax = () => {
   return (
     <section className="relative">
       <div className="relative ">
-        <div className="section text-8xl -m-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
-          <ParallaxContents />
+        <div className="section text-8xl -mt-44  h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
+          <ParallaxContents img={parallaxImg1}/>
         </div>
-        <div className="section text-8xl mt-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
-          <ParallaxContents />
+        <div className="section text-8xl  mt-64 h-[125vh] bg-[#089193] overflow-hidden sticky top-0">
+          <ParallaxContents img={parallaxImg2}/>
         </div>
-        <div className="section text-8xl mb-80  h-[100vh] bg-[#089193] overflow-hidden sticky top-0">
-          <ParallaxContents />
+        <div className="section text-8xl mt-64  h-[100vh] bg-[#089193] overflow-hidden sticky top-0">
+          <ParallaxContents img={parallaxImg1}/>
         </div>
       </div>
     </section>
