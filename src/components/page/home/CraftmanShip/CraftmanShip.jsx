a93bf1519d8f5d2d2b5903e7baece086e9225fe3
RF-Rifat <rfrifat6344@gmail.com>
2024-05-18 12:16:46 +0600

    lifestyle image hover and code polish completed

--- src/components/page/home/CraftmanShip/CraftmanShip.jsx
+++ src/components/page/home/CraftmanShip/CraftmanShip.jsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import React from "react";
 import Button from "./Button";
@@ -7,7 +6,7 @@ const Craftsmanship = () => {
   return (
     <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-[1220px] mx-auto">
       <div className="flex flex-col lg:flex-row gap-16">
-        <div className=" text-white mb-8 lg:mb-0 lg:w-1/2">
+        <div className="text-white mb-8 lg:mb-0 lg:w-1/2">
           <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-8 lg:mb-28 whitespace-nowrap mt-8">
             ACC Craftsmanship
           </h2>
@@ -26,32 +25,32 @@ const Craftsmanship = () => {
           </p>
           <Button />
         </div>
-        <div className="grid md:grid-cols-2 gap-6 lg:w-1/2">
-          <div className="flex justify-center">
+        <div className="grid md:grid-cols-2 gap-4 lg:w-1/2">
+          <div className="flex justify-center col-span-2 md:col-span-1">
             <Image
               width={500}
               height={500}
               src="/Rectangle4.png"
               alt="Image 1"
-              className="w-full h-80 rounded shadow-md"
+              className="w-full rounded shadow-md object-cover"
             />
           </div>
-          <div className="flex justify-center h-full">
+          <div className="flex justify-center col-span-2 md:col-span-1">
             <Image
               width={500}
               height={500}
               src="/Rectangle5.png"
               alt="Image 2"
-              className="rounded shadow-md lg:h-5/6"
+              className="w-full rounded shadow-md object-cover lg:h-5/6"
             />
           </div>
           <div className="flex justify-center col-span-2">
             <Image
-              width={500}
-              height={500}
+              width={600}
+              height={400}
               src="/Rectangle6.png"
               alt="Image 3"
-              className="w-full h-72 rounded shadow-md"
+              className="w-full rounded shadow-md object-cover"
             />
           </div>
         </div>
