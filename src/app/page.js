1ec551550a1e5421d675bccdf50b4bc9ad49312a
Forhad96 <forhadairdrop@gmail.com>
2024-05-19 08:15:33 +0600

    chek latest update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
 import Banner from "@/components/page/home/banner/Banner";
-import FlagshipProducts from "@/components/page/home/flagshipProducts/FlagshipProducts";
-import ProductSlider from "@/components/page/home/flagshipProducts/ProductSlider";
-import Newsletter from "@/components/shared/newsletter/Newsletter";
-import Fslider from "@/components/test/Fslider";
-import Fslider2 from "@/components/test/Fslider2/Fslider2";
 
 const Home = () => {
   return (
