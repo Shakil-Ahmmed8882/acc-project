77868e263e56cb6ec3043e67dba84b87c0e80628
RF-Rifat <rfrifat6344@gmail.com>
2024-05-21 14:49:39 +0600

    contact page contact modal serction added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,11 @@
+import AskQuestion from "@/components/page/contact/AskQuestion";
 import HeroSection from "@/components/shared/hero/Hero";
 
 const page = () => {
   return (
     <section>
+      <HeroSection />
+      <AskQuestion />
       <HeroSection />
     </section>
   );
