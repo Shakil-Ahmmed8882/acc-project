113f28ebea689428f0472299b28af7a5bbf01b95
RF-Rifat <rfrifat6344@gmail.com>
2024-05-27 15:13:10 +0600

    hetitage page ACC Craftsmanship hover effect added

--- src/app/craftsmanship/page.js
+++ src/app/craftsmanship/page.js
@@ -1,13 +1,18 @@
 import ImageGallery from "@/components/page/craftsmanship/imgGallery";
 import UnCraftsmanship from "@/components/page/craftsmanship/UnCraftsmanship";
 import HeroSection from "@/components/shared/hero/Hero";
+import hero1 from "@/assets/img/craftsmanship/banner/img1.png";
 
 const Heritage = () => {
   return (
     <section>
-          <HeroSection />
-          <UnCraftsmanship />
-          <ImageGallery/>
+      <HeroSection
+        title={"ACC Heritage"}
+        imagesArray={[hero1]}
+        noOverlay={true}
+      />
+      <UnCraftsmanship />
+      <ImageGallery />
     </section>
   );
 };
