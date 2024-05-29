0077a3443dff475c3dcdf033685042ca5d779fd2
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-05-29 20:01:15 +0700

    merge shakil/fix branch into dev

--- src/components/page/craftsmanship/content-container/HiddenContent.jsx
+++ src/components/page/craftsmanship/content-container/HiddenContent.jsx
@@ -2,12 +2,17 @@ const HiddenContent = ({ hiddenContent, isHovered }) => {
   const { title, description } = hiddenContent;
   return (
     <div
-      className={`${
+      className={`
+      ${
         isHovered ? 'opacity-100 visible translate-x-0' : 'invisible opacity-0 translate-x-32'
-      } absolute flex flex-col gap-5 transition-all duration-700 justify-center items-center inset-0 bg-[#01000083] w-full h-full z-50`}
+      }
+      absolute flex p-3 flex-col gap-5 transition-all duration-700 md:justify-center items-center inset-0 bg-[#01000083] w-full h-full z-50`}
     >
-      <h2 className="text-white text-4xl leading-10">{title || "Hello world"}</h2>
-      <div className="text-white leading-6">{description || "This is the description"}</div>
+      <div className="text-white p-3 absolute  md:text-center top-32 md:top-44 leading-6 ">
+      <h2 className={`text-white text-2xl md:text-3xl lg:text-4xl leading-10 `}>{title || "Hello world"}</h2>
+      <div className={`text-[#d1d1d1] pt-4 md:text-center leading-6 `}>{description || "This is the description"}</div>
+        
+      </div>
     </div>
   );
 };
--- src/components/page/craftsmanship/content-container/data.js
+++ src/components/page/craftsmanship/content-container/data.js
@@ -5,30 +5,38 @@ import composting from "@/assets/img/craftsmanship/imgGallery/craftsmanship4.png
 import cigarMaking from "@/assets/img/craftsmanship/imgGallery/craftsmanship5.png";
 import cigarStorage from "@/assets/img/craftsmanship/imgGallery/craftsmanship6.png";
 
-
 export const imgData = [
   {
-    id:1,
+    id: 1,
     img1: field,
     img2: seeding,
-    widh1: "60%",
+    width1: "60%",
     width2: "40%",
-    hiddenContent: { title: "I am title1", description: "i am description2" },
+    hiddenContent: { 
+      title: "Field Preparation",
+      description: "The initial stage where the soil is prepared for seeding"
+    },
   },
   {
-    id:2,
+    id: 2,
     img1: dryingLeaves,
     img2: composting,
-    widh1: "40%",
+    width1: "40%",
     width2: "60%",
-    hiddenContent: { title: "I am titl3", description: "i am descriptio3" },
+    hiddenContent: { 
+      title: "Drying Leaves & Composting",
+      description: "Leaves are dried to reduce moisture content, while composting helps in recycling organic "
+    },
   },
   {
-    id:3,
+    id: 3,
     img1: cigarMaking,
     img2: cigarStorage,
-    widh1: "60%",
+    width1: "60%",
     width2: "40%",
-    hiddenContent: { title: "I am titl4", description: "i am description4" },
+    hiddenContent: { 
+      title: "Cigar Making & Storage",
+      description: "The intricate process of hand-rolling cigars, followed by proper storage"
+    },
   },
 ];
