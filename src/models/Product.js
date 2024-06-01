d22d0e2e7e727c0149af62be2f0d884c1f205817
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-06-01 15:13:58 +0700

    default structure

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,6 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
+<<<<<<< Updated upstream
 const { Schema } = mongoose;
 
 
@@ -36,3 +37,59 @@ const Product =
   mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export { ProductType, Category, Product };
+=======
+// const ProductSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//     required: true,
+//   },
+//   description: {
+//     type: String,
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// const Product =
+//   mongoose.models.Product || mongoose.model("Product", ProductSchema);
+
+// export default Product;
+
+
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productTypeSchema = new Schema({
+  name: { type: String, required: true },
+  description: String,
+});
+
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+  description: String,
+  productType: { type: Schema.Types.ObjectId, ref: 'ProductType', required: true }
+});
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  productType: { type: Schema.Types.ObjectId, ref: 'ProductType', required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  images: [String]
+});
+
+const ProductType = mongoose.model('ProductType', productTypeSchema);
+const Category = mongoose.model('Category', categorySchema);
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { ProductType, Category, Product };
+>>>>>>> Stashed changes
