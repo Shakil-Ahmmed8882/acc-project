b43204294adf4643b5e8e3bc2d93b6d4c63acf4f
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-06-02 23:37:09 +0700

    fix(test):

--- src/components/page/products/tabs/Tabs.jsx
+++ src/components/page/products/tabs/Tabs.jsx
@@ -3,11 +3,11 @@ const Tabs= () => {
   return (
     <section>
         <ul className="flex gap-8 items-center text-white">
-            <li>tab1</li>
+            {/* <li>tab1</li>
             <li>tab2</li>
             <li>tab3</li>
             <li>tab4</li>
-            <li>tab5</li>
+            <li>tab5</li> */}
         </ul>
     </section>
   );
