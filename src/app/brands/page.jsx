89b7d5c4daf70a54a34da2db9268e6d3bafb210f
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-06-04 17:06:45 +0700

    add:brands tabs

--- src/app/brands/page.jsx
+++ src/app/brands/page.jsx
@@ -1,13 +1,16 @@
 "use client"
 
+import Tabs from "@/components/page/brands/Tabs";
 import BrandWorldAcc from "@/components/page/brands/brandWordAcc/BrandWorldAcc";
 import Banner from "@/components/page/home/banner/Banner";
 import AccLifestyle from "@/components/page/home/lifeStyle/LifeStyle";
-import Tabs from "@/components/page/products/acc-cigars/tabs/Tabs";
 import Newsletter from "@/components/shared/newsletter/Newsletter";
+
+
 
 const page = () => {
 
+  
   const titles = {
     title1:"Brand 1",
     title2:"Brand 2",
@@ -16,8 +19,8 @@ const page = () => {
   return (
     <div>
       <Banner titles={titles}/>
-      <Tabs/>
       <AccLifestyle />
+      <Tabs/>
       <BrandWorldAcc />
       <Newsletter />
     </div>
