7870f0887b13bcadf974b613c330b0b4fad0f995
RF-Rifat <rfrifat6344@gmail.com>
2024-06-05 10:46:05 +0600

    product data dynamically added by type

--- src/components/shared/parallax/ScrollParallax.jsx
+++ src/components/shared/parallax/ScrollParallax.jsx
@@ -4,6 +4,70 @@ import ParallaxContents from "./ParallaxContents";
 import parallaxImg1 from "@/assets/img/home/parallax/parallax1.png";
 import parallaxImg2 from "@/assets/img/home/parallax/parallax2.png";
 import parallaxImg3 from "@/assets/img/home/parallax/parallax3.png";
+
+import useGlobalContext from "@/hooks/useGlobalContext";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import AnimatedVideo from "../animation/animated-video/AnimatedVideo";
+
+const ScrollParallax = ({ titles }) => {
+  const { title1, title2, title3 } = titles || {};
+  const { isMenuOpen } = useGlobalContext();
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <section>
+      <div className="relative">
+        <div
+          className={`${
+            isMenuOpen ? "-mt-[800px] md:-mt-96" : "-mt-96"
+          } transition-all h-[125vh] duration-1000 section text-8xl overflow-hidden sticky top-0`}
+        >
+          <AnimatedVideo />
+          <ParallaxContents
+            img={parallaxImg1}
+            title={title1 || "Luxury & Vintage Cigars"}
+            page={0}
+          />
+        </div>
+        <div className="section smooth-transition text-8xl mt-64 h-screen overflow-hidden sticky top-0">
+          <ParallaxContents
+            img={parallaxImg2}
+            title={title2 || "Luxury & Vintage Spirits"}
+            page={1}
+          />
+        </div>
+        <div className="section smooth-transition text-8xl mt-64 h-screen overflow-hidden sticky top-0">
+          <ParallaxContents
+            img={parallaxImg3}
+            title={title3 || "luxury accessories"}
+            page={2}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ScrollParallax;
+
+
+
+
+
+
+
+
+
+
 // // import parallaxImg2 from "@/assets/img/home/parallax/parallax2";
 // // import parallaxImg3 from "@/assets/img/home/parallax/parallax3";
 // import AnimatedVideo from "../animation/animated-video/AnimatedVideo";
@@ -49,58 +113,3 @@ import parallaxImg3 from "@/assets/img/home/parallax/parallax3.png";
 // };
 
 // export default ScrollParallax;
-
-
-
-import useGlobalContext from "@/hooks/useGlobalContext";
-import { useEffect } from "react";
-import Lenis from "lenis";
-import AnimatedVideo from "../animation/animated-video/AnimatedVideo";
-
-const ScrollParallax = ({titles}) => {
-  const {title1,title2,title3} = titles || {}
-  const { isMenuOpen } = useGlobalContext();
-
-  // Initialize Lenis for smooth scrolling
-  useEffect(() => {
-    const lenis = new Lenis();
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf); // Call raf again for the next animation frame
-    }
-    requestAnimationFrame(raf); // Start the animation loop
-  }, []);
-
-  return (
-    <section>
-      {/* Add your smooth scrolling animation and parallax components here */}
-      <div className="relative">
-        <div
-          className={`${
-            isMenuOpen ? "-mt-[800px] md:-mt-96" : "-mt-96"
-          } transition-all h-[125vh] duration-1000 section text-8xl overflow-hidden sticky top-0`}
-        >
-          <AnimatedVideo/>
-          <ParallaxContents
-            img={parallaxImg1}
-            title={title1 || "LUXURY & VINTAGE CIGARS 1"}
-            page={0}
-          />
-        </div>
-        <div className="section smooth-transition text-8xl mt-64 h-screen overflow-hidden sticky top-0">
-          <ParallaxContents
-            img={parallaxImg2}
-            title={title2 || "LUXURY & VINTAGE CIGARS 2"}
-            page={1}
-          />
-        </div>
-        <div className="section smooth-transition text-8xl mt-64 h-screen overflow-hidden sticky top-0">
-          <ParallaxContents img={parallaxImg3}
-           title={title3 || "LUXURY & VINTAGE CIGARS 3"} page={2} />
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default ScrollParallax;
