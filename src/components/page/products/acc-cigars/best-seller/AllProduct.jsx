87628064f55cf80e2334ba8a9533de9697e5ac42
RF-Rifat <rfrifat6344@gmail.com>
2024-06-05 12:00:27 +0600

    product data dynamically added by type

--- src/components/page/products/acc-cigars/best-seller/AllProduct.jsx
+++ src/components/page/products/acc-cigars/best-seller/AllProduct.jsx
@@ -3,26 +3,41 @@ import Title from "../ui/Title";
 import { Loader } from "lucide-react";
 
 const AllProduct = ({ maxHeight, containerRef, product }) => {
+  const groupedProducts = groupProductsByCategory(product);
+
   return (
     <>
-      <section>
-        <Title title={"BEST SELLER"} />
-        <article
-          ref={containerRef}
-          className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-11 md:gap-20 lg:gap-24 transition-all duration-1000 ease-in-out overflow-hidden"
-          style={{ maxHeight }}
-        >
-          {product.length === 0 ? (
-            <Loader />
-          ) : (
-            product.map((card, index) => (
-              <ProductCard key={index} card={card} />
-            ))
-          )}
-        </article>
-      </section>
+      {Object.keys(groupedProducts).map((category) => (
+        <section key={category}>
+          <Title title={category} />
+          <article
+            ref={containerRef}
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-11 md:gap-20 lg:gap-24 transition-all duration-1000 ease-in-out overflow-hidden"
+            style={{ maxHeight }}
+          >
+            {groupedProducts[category].length === 0 ? (
+              <Loader />
+            ) : (
+              groupedProducts[category].map((card, index) => (
+                <ProductCard key={index} card={card} />
+              ))
+            )}
+          </article>
+        </section>
+      ))}
     </>
   );
+};
+
+const groupProductsByCategory = (products) => {
+  return products.reduce((acc, product) => {
+    const category = product.category;
+    if (!acc[category]) {
+      acc[category] = [];
+    }
+    acc[category].push(product);
+    return acc;
+  }, {});
 };
 
 export default AllProduct;
