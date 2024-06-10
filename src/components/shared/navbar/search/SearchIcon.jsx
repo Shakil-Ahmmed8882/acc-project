ffd56cbc4692c17b49258684b29f16506106917b
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-06-11 00:42:30 +0700

    add search icon

--- src/components/shared/navbar/search/SearchIcon.jsx
+++ src/components/shared/navbar/search/SearchIcon.jsx
@@ -1,14 +1,17 @@
 import { Search } from 'lucide-react';
-
+import searchicon from '@/assets/img/shared/navbar/searchicon.png'
+import Image from 'next/image';
 
 // Component for the Search icon with transition and click handler
 const SearchIcon = ({ isSwap, onClick }) => {
   return (
-    <Search
+    <Image
       onClick={onClick}
+      src={searchicon}
       className={`
-        font-thin
-        ${isSwap ? ' mb-2 translate-x-44 md:translate-x-16' : ' -translate-x-0 md:-translate-x-3'} justify-self-end pb-1 z-30 transition-all p-[3px] md:p-0 duration-1000`}
+        md:w-[17px]
+        ${isSwap ? ' mb-2 translate-x-44 md:translate-x-16' : ' -translate-x-0 md:-translate-x-5'} justify-self-end pb-1 z-30 transition-all p-[3px] md:p-0 duration-1000`}
+        alt=''
     />
   );
 };
