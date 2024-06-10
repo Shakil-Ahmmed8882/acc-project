3e952288e8015e98b838cd9a38d4130b290df287
RF-Rifat <rfrifat6344@gmail.com>
2024-06-10 11:52:08 +0600

    lifeStyle and craftmanship updated

--- src/components/page/contact/Modal.jsx
+++ src/components/page/contact/Modal.jsx
@@ -2,7 +2,6 @@ import { motion, AnimatePresence } from "framer-motion";
 import { CircleX } from "lucide-react";
 import FormInput from "./FormInput";
 
-
 const Modal = ({ isOpen, onClose }) => {
   return (
     <AnimatePresence>
@@ -12,8 +11,12 @@ const Modal = ({ isOpen, onClose }) => {
             initial={{ y: "100%" }}
             animate={{ y: 0 }}
             exit={{ y: "120%" }}
-            transition={{ type: "spring", stiffness: 200, damping: 20 }}
-            className="text-white rounded-lg shadow-lg w-ful overflow-hidden bg-[#121212] overflow-y-scroll"
+            transition={{
+              type: "tween",
+              ease: "easeIn",
+              duration: 0.4,
+            }}
+            className="text-white rounded-lg shadow-lg w-full overflow-hidden bg-[#121212] overflow-y-scroll"
           >
             <div className="grid place-items-center w-full">
               <button
@@ -23,8 +26,8 @@ const Modal = ({ isOpen, onClose }) => {
                 <CircleX className="size-16" />
               </button>
             </div>
-            <div className="p-6 max-w-2xl mx-auto">
-              <h2 className="text-4xl my-10 text-center">
+            <div className="p-6 max-w-3xl mx-auto">
+              <h2 className="text-5xl uppercase my-10 text-center font-riviera">
                 Request Information
               </h2>
               {/* form input field component */}
@@ -36,5 +39,5 @@ const Modal = ({ isOpen, onClose }) => {
     </AnimatePresence>
   );
 };
-export default Modal;
 
+export default Modal;
