5d77dfe4706d9a28034845b258eb77e2322e688b
RF-Rifat <rfrifat6344@gmail.com>
2024-06-15 21:53:02 +0600

    scroll issue

--- src/components/page/home/banner/Banner.jsx
+++ src/components/page/home/banner/Banner.jsx
@@ -1,14 +1,38 @@
 "use client";
 import useScrollObserver from "@/hooks/useScrollObserver";
 import ScrollParallax from "@/components/shared/parallax/ScrollParallax";
+import { useEffect } from "react";
+import Lenis from "lenis";
 // import { useEffect } from "react";
 // import Lenis from "lenis";
 
-// pass dynamic titles object images array 
-const Banner = ({titles,images}) => {
-
-  
+// pass dynamic titles object images array
+const Banner = ({ titles, images }) => {
   useScrollObserver();
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 0.2, // Controls the duration of the smooth scroll animation
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
+      direction: "vertical", // Scroll direction, could be 'vertical' or 'horizontal'
+      gestureDirection: "vertical", // Direction for touch gestures
+      smooth: true, // Enable or disable smooth scrolling
+      smoothTouch: true, // Disable smooth touch scrolling
+      touchMultiplier: 2, // Multiplier for touch scrolling
+      infinite: false, // Whether to loop the scroll
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy(); // Clean up Lenis instance on component unmount
+    };
+  }, []);
 
   // useEffect(() => {
   //   const lenis = new Lenis();
@@ -23,10 +47,10 @@ const Banner = ({titles,images}) => {
   //     lenis.destroy();
   //   };
   // }, []);
-  
+
   return (
     <main>
-      <ScrollParallax images={images} titles={titles}/>
+      <ScrollParallax images={images} titles={titles} />
     </main>
   );
 };
