f438ec2f89de87d5ad121f4656caca85520945bd
RF-Rifat <rfrifat6344@gmail.com>
2024-09-05 14:58:15 +0600

    video player updated

--- src/components/shared/navbar/animation.js
+++ src/components/shared/navbar/animation.js
@@ -1,7 +1,5 @@
-
-
- export const headerVariants = {
+export const headerVariants = {
   initial: { opacity: 1, translateY: 0 },
-  scrollingDown: { opacity: 0, translateY: -32 },
+  scrollingDown: { opacity: 0, translateY: -32, display: "none" },
   scrollingUp: { opacity: 1, translateY: 0 },
 };
